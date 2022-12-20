9300f32e494b6f8dab2d9368620f78795bc27c62
shijin-benny <mr.shijinbenny361@gmail.com>
2022-12-20 15:57:53 +0530

    remove twilio module

--- src/auth/otpService.ts
+++ src/auth/otpService.ts
@@ -17,7 +17,7 @@ import { Model, ObjectId } from 'mongoose';
 import { OtpReason, Status } from '../models/Enums';
 import { Otp, otpDocument } from '../schemas/otp.schema';
 import { verifyMobileDto } from './dto/auth.dto';
-import { InjectTwilio, TwilioClient } from 'nestjs-twilio';
+// import { InjectTwilio, TwilioClient } from 'nestjs-twilio';
 const SMS_API = `682b19a3-7047-11eb-a9bc-0200cd936042`;
 
 @Injectable()
@@ -25,8 +25,8 @@ export class otpService {
   constructor(
     private http: HttpService,
     @InjectModel(Otp.name) private otpModel: Model<otpDocument>,
-    @InjectTwilio() private readonly client: TwilioClient,
-  ) {}
+  ) // @InjectTwilio() private readonly client: TwilioClient,
+  {}
 
   async sentOtpMobile(phone: string, reason: OtpReason) {
     try {
