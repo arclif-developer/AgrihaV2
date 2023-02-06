2be6974fdbc21874968a64df7dc6eb054c6403dd
shijin-benny <mr.shijinbenny361@gmail.com>
2023-02-06 15:56:26 +0530

    user request product api added

--- src/Ecommerce/product/dto/create-product.dto.ts
+++ src/Ecommerce/product/dto/create-product.dto.ts
@@ -1,1 +1,15 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class Req_productDetails {
+  @IsString()
+  image: string;
+
+  @IsString()
+  product_name: string;
+
+  @IsString()
+  product_category: string;
+
+  @IsString()
+  creator: string;
+}
--- src/Ecommerce/product/product.module.ts
+++ src/Ecommerce/product/product.module.ts
@@ -3,11 +3,21 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Product, ProductSchema } from '../../schemas/product.schema';
-import { review, reviewSchema } from '../../schemas/reviews.schema';
+
+import {
+  ReqProducts,
+  ReqProductsSchema,
+} from '../../schemas/reqProducts.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    MongooseModule.forFeature(
+      [
+        { name: Product.name, schema: ProductSchema },
+        { name: ReqProducts.name, schema: ReqProductsSchema },
+      ],
+      'AGRIHA_DB',
+    ),
   ],
   controllers: [ProductController],
   providers: [ProductService],
