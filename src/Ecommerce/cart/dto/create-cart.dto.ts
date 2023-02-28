aea35a0085733d73ee4cbf8d0b004e78cb23fa53
shijin-benny <mr.shijinbenny361@gmail.com>
2023-02-28 12:10:55 +0530

    cart create api updated

--- src/Ecommerce/cart/dto/create-cart.dto.ts
+++ src/Ecommerce/cart/dto/create-cart.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateCartDto {
+  @ApiProperty({ example: '634e5330c5b0e4e57043e97b', required: true })
   @IsString()
   @IsNotEmpty()
   product_id: string;
 
+  @ApiProperty({ example: '634e5330c5b0e4e57043e97b', required: true })
   @IsString()
   @IsNotEmpty()
   user_id: string;
 
+  @ApiProperty({ example: 2, required: true })
   @IsNumber()
   quantity: number;
 }
