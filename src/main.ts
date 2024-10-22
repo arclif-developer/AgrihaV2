b07726e316dd39de94d5100162cc22095e2e16a1
arclif-developer <73050824+arclif-developer@users.noreply.github.com>
2024-10-22 23:21:10 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     console.log('Hello world listening on port', port);
   });
